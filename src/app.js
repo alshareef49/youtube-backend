9c0de0c54aa7ecc89cafdb5d7903d60dbb5fd7e4
alshareef49 <alshareef4039@gmail.com>
2024-01-07 01:48:57 +0530

    fixed bug

--- src/app.js
+++ src/app.js
@@ -2,14 +2,15 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-app = express();
+const app = express();
 app.use(cors({
     origin: process.env.CROSS_ORIGIN,
     credentials: true
 }))
 app.use(express.json({limit:"16kb"}));
-app.use(urlencoded({extended,limit:"16kb"}));
+app.use(urlencoded({extended: true,limit:"16kb"}));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 
 export  {app};

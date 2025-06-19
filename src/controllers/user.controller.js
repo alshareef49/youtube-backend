9f835af927f0f2b816ec18fd9a25e5843d373f61
Shareef Alam <alshareef4039@gmail.com>
2025-06-19 11:48:57 +0530

    refactor code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,8 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
-import mongoose from "mongoose";
-import { ObjectId } from "mongodb";
+
 
 const generateAccessAndRefereshTokens = async (userId) => {
   try {
